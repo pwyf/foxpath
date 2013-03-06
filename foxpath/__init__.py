7b0302bae42d28b6f4e4f2e0db89c0eeddb13766
Martin Keegan <martin@no.ucant.org>
2013-03-06 21:34:26 +0000

    initial commit

---
+++ foxpath/__init__.py
@@ -0,0 +1,1 @@
+from core import generate_test_functions, TestSyntaxError
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from setuptools import setup
+
+setup(
+	name = "foxpath",
+	author = "Martin Keegan",
+	author_email = "martin.keegan@okfn.org",
+	version = "0.1",
+	license = "GNU Affero General Public License v3.0",
+	url = "",
+	download_url = "",
+	description = "Python library for running FoXPath tests against XML",
+	py_modules = "",
+	packages = ["foxpath"],
+	install_requires = "",
+	scripts = ""
+)
