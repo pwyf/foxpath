f183b59cd0931df808b75f899689c6bc9c9722db
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-08-01 04:00:12 +0100

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	name = "foxpath",
 	author = "Martin Keegan",
 	author_email = "martin.keegan@okfn.org",
-	version = "0.8",
+	version = "0.9",
 	license = "GNU Affero General Public License v3.0",
 	url = "",
 	download_url = "",
