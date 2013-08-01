04cd203c3a137dd7eeece63a38a542fad98c4eb3
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-08-01 04:23:57 +0100

    Add error handling back in

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	name = "foxpath",
 	author = "Martin Keegan",
 	author_email = "martin.keegan@okfn.org",
-	version = "0.9",
+	version = "0.91",
 	license = "GNU Affero General Public License v3.0",
 	url = "",
 	download_url = "",
