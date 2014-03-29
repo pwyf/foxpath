725df2c894cba3321cfdfbbafbf9568c23eb1471
Mark Brough <mark.brough@publishwhatyoufund.org>
2014-03-29 20:04:52 +0000

    Check another expression

--- list_test.py
+++ list_test.py
@@ -3,6 +3,7 @@ from foxpath import test
 #f="dfid-tz.xml"
 f='sida-tz.xml'
 #t='at least one (sector[@vocabulary="DAC"]/@code or sector[not(@vocabulary)]/@code) is on list Sector?'
-t='at least one country-budget-items[@vocabulary="1"]/budget-item/@code is on list BudgetIdentifier?'
-lists={'Sector': ['13040'], 'BudgetIdentifier': ['5.1.1']}
+#t='at least one country-budget-items[@vocabulary="1"]/budget-item/@code is on list BudgetIdentifier?'
+t="default-aid-type/@code or transaction/aid-type/@code is on list AidType (if activity-status/@code is at least 2)?"
+lists={'Sector': ['13040'], 'BudgetIdentifier': ['5.1.1'], 'AidType': ['A01']}
 test.test_doc_lists(f,t,lists)
