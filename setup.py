98013504b8db6b3af94d285678404ce6039c1c16
markbrough <mark.brough@publishwhatyoufund.org>
2014-06-03 23:16:56 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	name = "foxpath",
 	author = "Martin Keegan",
 	author_email = "martin.keegan@okfn.org",
-	version = "0.99.11",
+	version = "0.99.12",
 	license = "GNU Affero General Public License v3.0",
 	url = "",
 	download_url = "",
