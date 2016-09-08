5bc1c4395628a030bea19f6725645717c07df43e
Andy Lulham <a.lulham@gmail.com>
2016-09-08 16:47:48 +0100

    test_doc should call test_doc_json_out

--- foxpath/test.py
+++ foxpath/test.py
@@ -44,29 +44,13 @@ def generate_function(test):
     return f
 
 def test_doc(filename, test):
-    test_fn=generate_function(test)
-    doc = etree.parse(filename)
-    activities=doc.xpath("//iati-activity")
-    success =0
-    fail = 0
-    error = 0
-    notrelevant = 0
-    for activity in activities:
-        result = test_fn(activity)
-        if result == 0:
-            fail +=1
-        elif result == 1:
-            success +=1
-        elif result == 2:
-            error +=1
-        elif result == None:
-            notrelevant +=1
+    j = test_doc_json_out(filename, test)
     print(test)
-    print("Success: {}".format(success))
-    print("Fail: {}".format(fail))
-    print("Error: {}".format(error))
-    print("Not relevant: {}".format(notrelevant))
-    print("Percentage: {}".format(float(success)/float(success+fail)*100.0))
+    print("Success: {}".format(j["summary"]["success"]))
+    print("Fail: {}".format(j["summary"]["fail"]))
+    print("Error: {}".format(j["summary"]["error"]))
+    print("Not relevant: {}".format(j["summary"]["not_relevant"]))
+    print("Percentage: {}".format(j["summary"]["percentage"]))
 
 def result_t(result_value):
     results = {0: "FAIL",
@@ -81,10 +65,11 @@ def binary_test(test_name):
         return True
     return False
 
-def test_doc_json_out(filename, test, current_test, lists=None):
+def test_doc_json_out(filename, test, current_test=None, lists=None):
     data = {}
     test_fn=generate_function(test)
-    current_test_fn = generate_function(current_test)
+    if current_test:
+        current_test_fn = generate_function(current_test)
     doc = etree.parse(filename)
     activities=doc.xpath("//iati-activity")
     success =0
@@ -102,10 +87,13 @@ def test_doc_json_out(filename, test, current_test, lists=None):
                 result = test_fn(activity)
         except Exception:
             result = 2
-        try:
-            current_result = current_test_fn(activity)
-        except Exception:
-            current_result = 2
+        if current_test:
+            try:
+                current_result = current_test_fn(activity)
+            except Exception:
+                current_result = 2
+        else:
+            current_result = None
 
         if result == 0:
             fail +=1
@@ -115,7 +103,7 @@ def test_doc_json_out(filename, test, current_test, lists=None):
             error +=1
         elif result == None:
             notrelevant +=1
-        
+
         result_text = result_t(result)
         current_text = result_t(current_result)
         try:
