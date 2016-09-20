e2ddfaca07d4a7bee35c7bcb9f2c38abe9aeb37b
Andy Lulham <a.lulham@gmail.com>
2016-09-20 10:54:20 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	name = "foxpath",
 	author = "Martin Keegan",
 	author_email = "martin.keegan@okfn.org",
-	version = "0.99.23",
+	version = "0.99.24",
 	license = "GNU Affero General Public License v3.0",
 	url = "",
 	download_url = "",
