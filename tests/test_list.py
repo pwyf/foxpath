a9bf3a78fd285a01ad52905915bacb37dac4b401
Andy Lulham <a.lulham@gmail.com>
2017-01-04 12:47:50 +0000

    Turn the included tests into unit tests

---
+++ tests/test_list.py
@@ -0,0 +1,38 @@
+import os.path
+from unittest import TestCase
+
+import foxpath
+from foxpath import test
+
+
+class TestLists(TestCase):
+    LISTS = {'Sector': ['13040'], 'BudgetIdentifier': ['5.1.1'], 'AidType': ['A01']}
+    FILEPATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sida-tz.xml')
+
+    def test_at_least_one_a_or_b_is_on_list_c(self):
+        t = 'at least one sector[@vocabulary="DAC"]/@code or sector[not(@vocabulary)]/@code is on list Sector?'
+        result = test.test_doc_json_out(self.FILEPATH, t, lists=self.LISTS)
+        assert result['summary']['success'] == 20
+        assert result['summary']['fail'] == 527
+        assert result['summary']['not_relevant'] == 0
+
+    def test_at_least_one_a_is_on_list_b_if_c_is_at_least_d_and_e_or_f_is_not_g_or_h(self):
+        t = 'at least one country-budget-items[@vocabulary="1"]/budget-item/@code is on list BudgetIdentifier (if activity-status/@code is at least 2 and (default-aid-type/@code or transaction/aid-type/@code is not A01 or A02))?'
+        result = test.test_doc_json_out(self.FILEPATH, t, lists=self.LISTS)
+        assert result['summary']['success'] == 48
+        assert result['summary']['fail'] == 485
+        assert result['summary']['not_relevant'] == 14
+
+    def test_at_least_one_a_is_on_list_b(self):
+        t = 'at least one country-budget-items[@vocabulary="1"]/budget-item/@code is on list BudgetIdentifier?'
+        result = test.test_doc_json_out(self.FILEPATH, t, lists=self.LISTS)
+        assert result['summary']['success'] == 48
+        assert result['summary']['fail'] == 499
+        assert result['summary']['not_relevant'] == 0
+
+    def test_a_or_b_is_on_list_c_if_d_is_at_least_e(self):
+        t = 'default-aid-type/@code or transaction/aid-type/@code is on list AidType (if activity-status/@code is at least 2)?'
+        result = test.test_doc_json_out(self.FILEPATH, t, lists=self.LISTS)
+        assert result['summary']['success'] == 2
+        assert result['summary']['fail'] == 545
+        assert result['summary']['not_relevant'] == 0
