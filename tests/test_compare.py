0c580e08a21c28f5e5908141b7c132a7634709ec
Andy Lulham <a.lulham@gmail.com>
2017-02-09 17:39:45 +0000

    Also compare old and new on DFID data

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -32,20 +32,27 @@ class TestCompare(TestCase):
                 for indicator in reader for t in indicator['tests']
             ]
 
-    FILEPATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sida-tz.xml')
     LISTS = codelists.CODELISTS
 
-    def test_compare(self):
+    def run_comparison(self, filename):
+        filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
+
         tests_old = self.load_expressions_from_csvfile(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'old_tests.csv'))
-        result_old = test.test_doc(self.FILEPATH, tests_old, lists=self.LISTS)
+        result_old = test.test_doc(filepath, tests_old, lists=self.LISTS)
         summary_old = result_old['summary']
 
         tests_new = self.load_expressions_from_yaml(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tests.yaml'))
         foxpath = Foxpath(tests_new, self.LISTS)
-        result_new = foxpath.test_doc(self.FILEPATH)
+        result_new = foxpath.test_doc(filepath)
         summary_new = foxpath.summarize_results(result_new)
 
         for k, v in summary_old.items():
             self.assertEqual(summary_new[k]['fail'], v['fail'])
             self.assertEqual(summary_new[k]['pass'], v['success'])
             self.assertEqual(summary_new[k]['not-relevant'], v['not_relevant'])
+
+    def test_compare_sida(self):
+        self.run_comparison('sida-tz.xml')
+
+    def test_compare_dfid(self):
+        self.run_comparison('dfid-tz.xml')
