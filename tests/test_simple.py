5c5111962afa0cd1032fff695186c35b222d3bf8
Andy Lulham <a.lulham@gmail.com>
2017-02-09 02:30:15 +0000

    Update test_simple.py to use new version

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,79 +1,90 @@
 import os.path
 from unittest import TestCase
-from datetime import datetime
+from datetime import date
 
 from mock import patch
 
-from foxpath import test
+from foxpath import Foxpath
 
 
 class TestSimple(TestCase):
     FILEPATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dfid-tz.xml')
 
     def test_a_starts_with_b(self):
-        t = 'iati-identifier/text() starts with reporting-org/@ref?'
-        result = test.test_doc(self.FILEPATH, [t])
-        self.assertEqual(result['summary'][t]['success'], 273)
-        self.assertEqual(result['summary'][t]['fail'], 0)
-        self.assertEqual(result['summary'][t]['not_relevant'], 0)
-        self.assertEqual(result['summary'][t]['error'], 0)
+        t = {
+            'id': '_',
+            'expression': '`iati-identifier/text()` starts with `reporting-org/@ref`',
+        }
+        foxpath = Foxpath([t])
+        result = foxpath.test_doc(self.FILEPATH)
+        summary = foxpath.summarize_results(result)
+        self.assertEqual(summary[t['id']]['pass'], 273)
+        self.assertEqual(summary[t['id']]['fail'], 0)
+        self.assertEqual(summary[t['id']]['not-relevant'], 0)
 
-    @patch('foxpath.mapping.datetime.datetime')
-    def test_a_or_b_or_c_or_d_or_e_for_any_f_is_less_than_g_months_ago(self, mock_datetime):
-        mock_datetime.now.return_value = datetime(2015, 12, 1)
-        mock_datetime.strptime.side_effect = lambda *args, **kw: datetime.strptime(*args, **kw)
+    @patch('foxpath.mapping.datetime.date')
+    def test_a_or_b_or_c_or_d_or_e_for_any_f_is_less_than_g_months_ago(self, mock_date):
+        mock_date.today.return_value = date(2015, 12, 1)
 
-        t = 'activity-date[@type="end-planned"]/@iso-date or activity-date[@type="end-planned"]/text() or activity-date[@type="end-actual"]/@iso-date or activity-date[@type="end-actual"]/text() or transaction-date/@iso-date (for any transaction[transaction-type/@code="D"]|transaction[transaction-type/@code="E"]) is less than 13 months ago?'
-        result = test.test_doc(self.FILEPATH, [t])
-        self.assertEqual(result['summary'][t]['success'], 132)
-        self.assertEqual(result['summary'][t]['fail'], 141)
-        self.assertEqual(result['summary'][t]['not_relevant'], 0)
-        self.assertEqual(result['summary'][t]['error'], 0)
+        t = {
+            'id': '_',
+            'expression': '`activity-date[@type="end-planned"]/@iso-date|activity-date[@type="end-planned"]/text()` is less than 12 months ago or `activity-date[@type="end-actual"]/@iso-date|activity-date[@type="end-actual"]/text()` is less than 12 months ago or for any `transaction[transaction-type/@code="D"]|transaction[transaction-type/@code="E"]`, `transaction-date/@iso-date` is less than 12 months ago',
+        }
+        foxpath = Foxpath([t])
+        result = foxpath.test_doc(self.FILEPATH)
+        summary = foxpath.summarize_results(result)
+        self.assertEqual(summary[t['id']]['pass'], 132)
+        self.assertEqual(summary[t['id']]['fail'], 141)
+        self.assertEqual(summary[t['id']]['not-relevant'], 0)
 
     def test_a_or_b_exists_if_c_is_at_least_d_and_e_is_not_f(self):
-        t = 'conditions or document-link/category[@code="A04"] exists (if activity-status/@code is at least 2 and conditions/@attached is not 0)?'
-        result = test.test_doc(self.FILEPATH, [t])
-        self.assertEqual(result['summary'][t]['success'], 13)
-        self.assertEqual(result['summary'][t]['fail'], 178)
-        self.assertEqual(result['summary'][t]['not_relevant'], 82)
-        self.assertEqual(result['summary'][t]['error'], 0)
+        t = {
+            'id': '_',
+            'expression': 'if `activity-status/@code` is at least 2 and `conditions/@attached` is not 0 then `conditions` exists or `document-link/category[@code="A04"]` exists',
+        }
+        foxpath = Foxpath([t])
+        result = foxpath.test_doc(self.FILEPATH)
+        summary = foxpath.summarize_results(result)
+        self.assertEqual(summary[t['id']]['pass'], 13)
+        self.assertEqual(summary[t['id']]['fail'], 178)
+        self.assertEqual(summary[t['id']]['not-relevant'], 82)
 
-    @patch('foxpath.mapping.datetime.datetime')
-    def test_a_or_b_is_available_forward_if_c_is_at_least_d(self, mock_datetime):
-        mock_datetime.now.return_value = datetime(2015, 12, 1)
-        mock_datetime.strptime.side_effect = lambda *args, **kw: datetime.strptime(*args, **kw)
+    @patch('foxpath.mapping.datetime.date')
+    def test_a_or_b_is_available_forward_if_c_is_at_least_d(self, mock_date):
+        mock_date.today.return_value = date(2015, 12, 1)
+        t = {
+            'id': '_',
+            'expression': 'if `activity-status/@code` is at least 2 then `budget` is available forward or `planned-disbursement` is available forward',
+        }
+        foxpath = Foxpath([t])
+        result = foxpath.test_doc(self.FILEPATH)
+        summary = foxpath.summarize_results(result)
+        self.assertEqual(summary[t['id']]['pass'], 20)
+        self.assertEqual(summary[t['id']]['fail'], 14)
+        self.assertEqual(summary[t['id']]['not-relevant'], 239)
 
-        t = 'budget or planned-disbursement is available forward (if activity-status/@code is at least 2)?'
-        result = test.test_doc(self.FILEPATH, [t])
-        self.assertEqual(result['summary'][t]['success'], 20)
-        self.assertEqual(result['summary'][t]['fail'], 14)
-        self.assertEqual(result['summary'][t]['not_relevant'], 239)
-        self.assertEqual(result['summary'][t]['error'], 0)
-
-    @patch('foxpath.mapping.datetime.datetime')
-    def test_a_or_b_is_available_forward_by_quarters_if_c_is_at_least_d(self, mock_datetime):
-        mock_datetime.now.return_value = datetime(2015, 12, 1)
-        mock_datetime.strptime.side_effect = lambda *args, **kw: datetime.strptime(*args, **kw)
-
-        t = 'budget or planned-disbursement is available forward by quarters (if activity-status/@code is at least 2)?'
-        result = test.test_doc(self.FILEPATH, [t])
-        self.assertEqual(result['summary'][t]['success'], 0)
-        self.assertEqual(result['summary'][t]['fail'], 34)
-        self.assertEqual(result['summary'][t]['not_relevant'], 239)
-        self.assertEqual(result['summary'][t]['error'], 0)
-
-    def test_a_is_an_integer(self):
-        t = 'participating-org/@type is an integer?'
-        result = test.test_doc(self.FILEPATH, [t])
-        self.assertEqual(result['summary'][t]['success'], 273)
-        self.assertEqual(result['summary'][t]['fail'], 0)
-        self.assertEqual(result['summary'][t]['not_relevant'], 0)
-        self.assertEqual(result['summary'][t]['error'], 0)
+    @patch('foxpath.mapping.datetime.date')
+    def test_a_or_b_is_available_forward_by_quarters_if_c_is_at_least_d(self, mock_date):
+        mock_date.today.return_value = date(2015, 12, 1)
+        t = {
+            'id': '_',
+            'expression': 'if `activity-status/@code` is at least 2 then `budget` is available forward by quarters or `planned-disbursement` is available forward by quarters',
+        }
+        foxpath = Foxpath([t])
+        result = foxpath.test_doc(self.FILEPATH)
+        summary = foxpath.summarize_results(result)
+        self.assertEqual(summary[t['id']]['pass'], 0)
+        self.assertEqual(summary[t['id']]['fail'], 34)
+        self.assertEqual(summary[t['id']]['not-relevant'], 239)
 
     def test_a_exists_if_b_is_at_least_c_and_d_or_e_is_not_f_or_g(self):
-        t = 'capital-spend exists (if activity-status/@code is at least 2 and (default-aid-type/@code or transaction/aid-type/@code is not A01 or A02))?'
-        result = test.test_doc(self.FILEPATH, [t])
-        self.assertEqual(result['summary'][t]['success'], 0)
-        self.assertEqual(result['summary'][t]['fail'], 257)
-        self.assertEqual(result['summary'][t]['not_relevant'], 16)
-        self.assertEqual(result['summary'][t]['error'], 0)
+        t = {
+                'id': '_',
+                'expression': 'if `activity-status/@code` is at least 2 and `default-aid-type/@code` is not A01 and `default-aid-type/@code` is not A02 and `transaction/aid-type/@code` is not A01 and `transaction/aid-type/@code` is not A02 then `capital-spend` exists',
+            }
+        foxpath = Foxpath([t])
+        result = foxpath.test_doc(self.FILEPATH)
+        summary = foxpath.summarize_results(result)
+        self.assertEqual(summary[t['id']]['pass'], 0)
+        self.assertEqual(summary[t['id']]['fail'], 257)
+        self.assertEqual(summary[t['id']]['not-relevant'], 16)
---
+++ tests/test_simple_old.py
@@ -0,0 +1,79 @@
+import os.path
+from unittest import TestCase
+from datetime import datetime
+
+from mock import patch
+
+from foxpath import test
+
+
+class TestSimple(TestCase):
+    FILEPATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dfid-tz.xml')
+
+    def test_a_starts_with_b(self):
+        t = 'iati-identifier/text() starts with reporting-org/@ref?'
+        result = test.test_doc(self.FILEPATH, [t])
+        self.assertEqual(result['summary'][t]['success'], 273)
+        self.assertEqual(result['summary'][t]['fail'], 0)
+        self.assertEqual(result['summary'][t]['not_relevant'], 0)
+        self.assertEqual(result['summary'][t]['error'], 0)
+
+    @patch('foxpath.mapping.datetime.datetime')
+    def test_a_or_b_or_c_or_d_or_e_for_any_f_is_less_than_g_months_ago(self, mock_datetime):
+        mock_datetime.now.return_value = datetime(2015, 12, 1)
+        mock_datetime.strptime.side_effect = lambda *args, **kw: datetime.strptime(*args, **kw)
+
+        t = 'activity-date[@type="end-planned"]/@iso-date or activity-date[@type="end-planned"]/text() or activity-date[@type="end-actual"]/@iso-date or activity-date[@type="end-actual"]/text() or transaction-date/@iso-date (for any transaction[transaction-type/@code="D"]|transaction[transaction-type/@code="E"]) is less than 13 months ago?'
+        result = test.test_doc(self.FILEPATH, [t])
+        self.assertEqual(result['summary'][t]['success'], 132)
+        self.assertEqual(result['summary'][t]['fail'], 141)
+        self.assertEqual(result['summary'][t]['not_relevant'], 0)
+        self.assertEqual(result['summary'][t]['error'], 0)
+
+    def test_a_or_b_exists_if_c_is_at_least_d_and_e_is_not_f(self):
+        t = 'conditions or document-link/category[@code="A04"] exists (if activity-status/@code is at least 2 and conditions/@attached is not 0)?'
+        result = test.test_doc(self.FILEPATH, [t])
+        self.assertEqual(result['summary'][t]['success'], 13)
+        self.assertEqual(result['summary'][t]['fail'], 178)
+        self.assertEqual(result['summary'][t]['not_relevant'], 82)
+        self.assertEqual(result['summary'][t]['error'], 0)
+
+    @patch('foxpath.mapping.datetime.datetime')
+    def test_a_or_b_is_available_forward_if_c_is_at_least_d(self, mock_datetime):
+        mock_datetime.now.return_value = datetime(2015, 12, 1)
+        mock_datetime.strptime.side_effect = lambda *args, **kw: datetime.strptime(*args, **kw)
+
+        t = 'budget or planned-disbursement is available forward (if activity-status/@code is at least 2)?'
+        result = test.test_doc(self.FILEPATH, [t])
+        self.assertEqual(result['summary'][t]['success'], 20)
+        self.assertEqual(result['summary'][t]['fail'], 14)
+        self.assertEqual(result['summary'][t]['not_relevant'], 239)
+        self.assertEqual(result['summary'][t]['error'], 0)
+
+    @patch('foxpath.mapping.datetime.datetime')
+    def test_a_or_b_is_available_forward_by_quarters_if_c_is_at_least_d(self, mock_datetime):
+        mock_datetime.now.return_value = datetime(2015, 12, 1)
+        mock_datetime.strptime.side_effect = lambda *args, **kw: datetime.strptime(*args, **kw)
+
+        t = 'budget or planned-disbursement is available forward by quarters (if activity-status/@code is at least 2)?'
+        result = test.test_doc(self.FILEPATH, [t])
+        self.assertEqual(result['summary'][t]['success'], 0)
+        self.assertEqual(result['summary'][t]['fail'], 34)
+        self.assertEqual(result['summary'][t]['not_relevant'], 239)
+        self.assertEqual(result['summary'][t]['error'], 0)
+
+    def test_a_is_an_integer(self):
+        t = 'participating-org/@type is an integer?'
+        result = test.test_doc(self.FILEPATH, [t])
+        self.assertEqual(result['summary'][t]['success'], 273)
+        self.assertEqual(result['summary'][t]['fail'], 0)
+        self.assertEqual(result['summary'][t]['not_relevant'], 0)
+        self.assertEqual(result['summary'][t]['error'], 0)
+
+    def test_a_exists_if_b_is_at_least_c_and_d_or_e_is_not_f_or_g(self):
+        t = 'capital-spend exists (if activity-status/@code is at least 2 and (default-aid-type/@code or transaction/aid-type/@code is not A01 or A02))?'
+        result = test.test_doc(self.FILEPATH, [t])
+        self.assertEqual(result['summary'][t]['success'], 0)
+        self.assertEqual(result['summary'][t]['fail'], 257)
+        self.assertEqual(result['summary'][t]['not_relevant'], 16)
+        self.assertEqual(result['summary'][t]['error'], 0)
