27c1b22a27fa02de3b1d8cdc79ee751e4ef87f81
Andy Lulham <a.lulham@gmail.com>
2017-02-15 16:40:57 +0000

    Readd this because it is USEFUL

---
+++ tests/test_files.py
@@ -0,0 +1,47 @@
+import os.path
+from unittest import TestCase
+from datetime import date
+
+from mock import patch
+import yaml
+from lxml import etree
+
+from foxpath import Foxpath
+from tests import codelists
+
+
+'''
+Doesn't actually run any tests - this is purely here so I have some
+useful sample code!
+'''
+class TestFiles(TestCase):
+    def load_expressions_from_yaml(self, filename):
+        with open(filename) as f:
+            reader = yaml.load(f)
+            return [
+                {
+                    'id': t['description'],
+                    'expression': t['expression'],
+                }
+                for indicator in reader for t in indicator['tests']
+            ]
+
+    LISTS = codelists.CODELISTS
+
+    def run_tests(self, filename):
+        filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
+
+        tests = self.load_expressions_from_yaml(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tests.yaml'))
+        foxpath = Foxpath(tests, self.LISTS)
+        result = foxpath.test_doc(filepath)
+        summary = foxpath.summarize_results(result)
+
+    @patch('foxpath.mapping.datetime.date')
+    def test_compare_sida(self, mock_date):
+        mock_date.today.return_value = date(2015, 12, 1)
+        self.run_comparison('sidatz.xml')
+
+    @patch('foxpath.mapping.datetime.date')
+    def test_compare_dfid(self, mock_date):
+        mock_date.today.return_value = date(2015, 12, 1)
+        self.run_comparison('dfidtz.xml')
