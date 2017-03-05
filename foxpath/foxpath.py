0578969f4d94a425a60d7f44c64b28a44c51faef
Andy Lulham <a.lulham@gmail.com>
2017-03-05 19:28:32 +0300

    dict_values obj does not support indexing

--- foxpath/foxpath.py
+++ foxpath/foxpath.py
@@ -482,7 +482,7 @@ class Foxpath(object):
             -1: 0,
         }
         summary = [scores.copy() for x in range(len(activities_results[0]['results']))]
-        remove_explanations = type(activities_results[0]['results'].values()[0]) is not int
+        remove_explanations = type(list(activities_results[0]['results'].values())[0]) is not int
         for activity_results in activities_results:
             for test_idx, result in enumerate(activity_results['results'].values()):
                 if remove_explanations:
